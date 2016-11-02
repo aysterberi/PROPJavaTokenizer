062e2bcddf65610b138799a21a738db7d5bbbed1
Joakim Berglund <joakimberglund@live.se>
2016-11-02 19:19:30 +0100

    add more files

---
+++ src/prop/assignment0/Evaluator.java
@@ -0,0 +1,4 @@
+package prop.assignment0;
+
+public class Evaluator {
+}
---
+++ src/prop/assignment0/ExpressionNode.java
@@ -0,0 +1,14 @@
+package prop.assignment0;
+
+public class ExpressionNode implements INode {
+
+    @Override
+    public Object evaluate(Object[] args) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void buildString(StringBuilder builder, int tabs) {
+
+    }
+}
