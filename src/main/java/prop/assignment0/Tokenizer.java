7db70dcfa398ebea9a45d9ac70b00943d52b8146
Billy <chrysophylax@users.noreply.github.com>
2016-11-07 14:53:55 +0100

    Clearing up old code and imports

--- src/main/java/prop/assignment0/Tokenizer.java
+++ src/main/java/prop/assignment0/Tokenizer.java
@@ -1,20 +1,14 @@
 package prop.assignment0;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.LinkedList;
 
 public class Tokenizer implements ITokenizer {
 
 	public static final int MAX_IDENTIFIER_SIZE = 126;
 	private Scanner scanner;
-	private BufferedReader bufferedReader;
-	private LinkedList<Token> seenTokens;
 	private Lexeme currentLexeme;
 
-	public Tokenizer(String fileName) throws IOException, TokenizerException {
-		seenTokens = new LinkedList<>();
-		this.open(fileName);
+	public Tokenizer(){
 	}
 
 	@Override
@@ -35,26 +29,13 @@ public class Tokenizer implements ITokenizer {
 	public void moveNext() throws IOException, TokenizerException {
 		consumeWhitespace();
 		char c = scanner.current();
-		boolean tokenFound = false;
-		char[] chars = new char[126]; //limit identifier names to max 126 chars
-		int i = 0;
-		while ((chars[i] = scanner.current()) != Scanner.EOF) {
-			while (!tokenFound) {
-				chars[i] = scanner.current(); //fetch next char
-				if (chars[i] == ' ') { //have we hit whitespace or EOF?
-					tokenFound = true; //we've finished collecting a token
-				}
-				scanner.moveNext(); //get next char
-				i++; //increase array item ptr
-			}
-			tokenize(chars);
-		}
+//		tokenize(c);
 
 	}
 
 	@Override
 	public void close() throws IOException {
-
+		scanner.close();
 	}
 
 	private void consumeWhitespace() throws IOException {
@@ -63,58 +44,52 @@ public class Tokenizer implements ITokenizer {
 		}
 	}
 
-	private void tokenize(char[] chars) {
-		/*
-		We need to add support for arbitrary
-		length variable names.
-		Perhaps regex?
-		 */
-		Token current;
-		switch (chars[0]) {
-			case '{':
-				current = Token.LEFT_CURLY;
-				break;
-			case '}':
-				current = Token.RIGHT_CURLY;
-				break;
-			case '(':
-				current = Token.LEFT_PAREN;
-				break;
-			case ')':
-				current = Token.RIGHT_PAREN;
-				break;
-			case ';':
-				current = Token.SEMICOLON;
-				break;
-			case '+':
-				current = Token.ADD_OP;
-				break;
-			case '-':
-				current = Token.SUB_OP;
-				break;
-			case '/':
-				current = Token.DIV_OP;
-				break;
-			case '=':
-				current = Token.ASSIGN_OP;
-				break;
-			case '*':
-				current = Token.MULT_OP;
-				break;
-			default:
-				if (Character.isDigit((chars[0]))) {
-					current = Token.INT_LIT;
-
-					if (Character.isLowerCase(chars[0])) {
-						current = Token.IDENT;
-					}
-				}
-
-		}
-		//Lexeme lex = new Lexeme(chars[0], current);
-	//	currentLexeme = lex;
-	}
-
+	//	private void tokenize(char ch) {
+//		switch (ch) {
+//			case '{':
+//				current = Token.LEFT_CURLY;
+//				break;
+//			case '}':
+//				current = Token.RIGHT_CURLY;
+//				break;
+//			case '(':
+//				current = Token.LEFT_PAREN;
+//				break;
+//			case ')':
+//				current = Token.RIGHT_PAREN;
+//				break;
+//			case ';':
+//				current = Token.SEMICOLON;
+//				break;
+//			case '+':
+//				current = Token.ADD_OP;
+//				break;
+//			case '-':
+//				current = Token.SUB_OP;
+//				break;
+//			case '/':
+//				current = Token.DIV_OP;
+//				break;
+//			case '=':
+//				current = Token.ASSIGN_OP;
+//				break;
+//			case '*':
+//				current = Token.MULT_OP;
+//				break;
+//			default:
+//				if (Character.isDigit((chars[0]))) {
+//					current = Token.INT_LIT;
+//
+//					if (Character.isLowerCase(chars[0])) {
+//						current = Token.IDENT;
+//					}
+//				}
+//
+//		}
+//		//Lexeme lex = new Lexeme(chars[0], current);
+//		//	currentLexeme = lex;
+//	}
+//
 	private void constructIDENT() throws IOException {
 		char current = scanner.current();
 		int i = 0;
