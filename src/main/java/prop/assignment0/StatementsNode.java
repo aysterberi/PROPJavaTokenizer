9e2a4b3b5d005bcf852de56768469f67496a5a65
Billy <chrysophylax@users.noreply.github.com>
2016-11-08 19:32:58 +0100

    Init creation of statement logic

--- src/main/java/prop/assignment0/StatementsNode.java
+++ src/main/java/prop/assignment0/StatementsNode.java
@@ -5,6 +5,17 @@ public class StatementsNode implements INode {
     public INode statementsNode;
     @Override
     public Object evaluate(Object[] args) throws Exception {
+        Object result = null;
+        if (assignmentNode != null)
+        {
+            result = assignmentNode.evaluate(args);
+            //call eval and store result
+                //recurse eval next statement
+            if(statementsNode != null)
+            {
+
+            }
+        }
         return null;
     }
 
