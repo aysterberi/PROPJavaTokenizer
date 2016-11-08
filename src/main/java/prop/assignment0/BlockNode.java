3f6465db08b0d6b502b1c673c4d0fd674b4a3399
Billy <chrysophylax@users.noreply.github.com>
2016-11-08 19:32:41 +0100

    Ugly hack for BlockNode symbol table

--- src/main/java/prop/assignment0/BlockNode.java
+++ src/main/java/prop/assignment0/BlockNode.java
@@ -4,26 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BlockNode implements INode {
-    public Lexeme left_curly;
-    public Lexeme right_curly;
-    public INode right;
-    private Map<String, Object> varMap;
+	public Lexeme left_curly;
+	public Lexeme right_curly;
+	public INode right;
+	private Map<String, Object> varMap;
 
-    @Override
-    public Object evaluate(Object[] args) throws Exception {
-        //create a map to store IDENT and their calculations
-        varMap = new HashMap<>();
-        Object result = right.evaluate(args);
-        //
-        return result;
-    }
+	@Override
+	public Object evaluate(Object[] args) throws Exception {
+		//create a map to store IDENT and their calculations
+		varMap = new HashMap<>();
+		args[0] = varMap;
+		right.evaluate(args); //evaluate right
+		//
+		StringBuilder stringBuilder = new StringBuilder();
+		for (Map.Entry<String, Object> var : varMap.entrySet()) {
+			String varKey = var.getKey();
+			String varValue = var.getValue().toString();
+			stringBuilder.append(varKey).append("=").append(varValue);
+		}
+		return stringBuilder.toString();
+	}
 
-    @Override
-    public void buildString(StringBuilder builder, int tabs) {
-        builder.append("BlockNode\n").append(left_curly.toString()).append("\n");
-        tabs++;
-        if (right != null)
-            right.buildString(builder, tabs);
-        builder.append(right_curly.toString()).append("\n");
-    }
+	@Override
+	public void buildString(StringBuilder builder, int tabs) {
+		builder.append("BlockNode\n").append(left_curly.toString()).append("\n");
+		tabs++;
+		if (right != null)
+			right.buildString(builder, tabs);
+		builder.append(right_curly.toString()).append("\n");
+	}
 }
