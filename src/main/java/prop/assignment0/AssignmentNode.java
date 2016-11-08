75890550f25ba3ff9571b25e9887b842285c5338
Billy <chrysophylax@users.noreply.github.com>
2016-11-08 20:00:59 +0100

    Maybe this works, maybe it doesn't. After exam, you will know.

--- src/main/java/prop/assignment0/AssignmentNode.java
+++ src/main/java/prop/assignment0/AssignmentNode.java
@@ -1,5 +1,7 @@
 package prop.assignment0;
 
+import java.util.Map;
+
 public class AssignmentNode implements INode {
     public Lexeme ident;
     public Lexeme operator;
@@ -7,7 +9,20 @@ public class AssignmentNode implements INode {
     public Lexeme semicolon;
     @Override
     public Object evaluate(Object[] args) throws Exception {
-        return null;
+        //create var ident
+        String identifier = ident.value().toString();
+        //eval var ident
+        Object varEval = expressionNode.evaluate(args);
+        //extract symbol table from args
+        //store results of eval with ident
+        if(args[0] instanceof Map)
+        {
+            Map<String, Object> varMap = (Map) args[0];
+            varMap.put(identifier, varEval);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(identifier).append("=").append(varEval);
+        return sb.toString();
     }
 
     @Override
