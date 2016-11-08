e4b8f5586a1746fe7a9da73cdf2eca8b43ff3875
Billy <chrysophylax@users.noreply.github.com>
2016-11-08 18:40:11 +0100

    FactorNode evaluate()

--- src/main/java/prop/assignment0/FactorNode.java
+++ src/main/java/prop/assignment0/FactorNode.java
@@ -1,17 +1,34 @@
 package prop.assignment0;
 
 public class FactorNode implements INode {
-    public Lexeme leftParen;
+	public Lexeme leftParen;
 	public Lexeme rightParen;
 	public Lexeme ident;
 	public INode expressionNode;
+
 	@Override
-    public Object evaluate(Object[] args) throws Exception {
-        return null;
-    }
+	public Object evaluate(Object[] args) throws Exception {
+		Object result = null;
 
-    @Override
-    public void buildString(StringBuilder builder, int tabs) {
+		if (ident != null) {
+			switch (ident.token()) {
+				case INT_LIT:
+					result = ident.value();
+					break;
+				case IDENT:
+					String s = (String) ident.value();
+					result = s;
+					break;
+			}
+		} else if (expressionNode != null) {
+			//recurse expr eval
+			result = expressionNode.evaluate(args);
+		}
+		return result;
+	}
 
-    }
+	@Override
+	public void buildString(StringBuilder builder, int tabs) {
+
+	}
 }
