ff4f2391c372a38cf40ad7e4e2579be0d0898f41
Joakim Berglund <joakimberglund@live.se>
2016-11-08 22:06:07 +0100

    fix indentation

--- src/main/java/prop/assignment0/BlockNode.java
+++ src/main/java/prop/assignment0/BlockNode.java
@@ -12,13 +12,21 @@ public class BlockNode implements INode {
 
     @Override
     public void buildString(StringBuilder builder, int tabs) {
-        builder.append("BlockNode\n").
-                append(left_curly).
-                append("\n");
+        builder.append("BlockNode\n");
         tabs++;
+        applyTabs(builder, tabs);
+        builder.append(left_curly).
+                append("\n");
         if (right != null)
             right.buildString(builder, tabs);
+        applyTabs(builder, tabs);
         builder.append(right_curly).
                 append("\n");
+    }
+
+    private void applyTabs(StringBuilder builder, int tabs) {
+        for (int i = 0; i < tabs; i++) {
+            builder.append("\t");
+        }
     }
 }
