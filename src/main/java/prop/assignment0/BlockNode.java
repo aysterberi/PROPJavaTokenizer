eb03422d6df1e83c85e98fe33b42c351fe330ed1
Joakim Berglund <joakimberglund@live.se>
2016-11-08 18:22:32 +0100

    begin buildString method

--- src/main/java/prop/assignment0/BlockNode.java
+++ src/main/java/prop/assignment0/BlockNode.java
@@ -4,6 +4,7 @@ public class BlockNode implements INode {
     public Lexeme left_curly;
     public Lexeme right_curly;
     public INode right;
+
     @Override
     public Object evaluate(Object[] args) throws Exception {
         return null;
@@ -11,6 +12,10 @@ public class BlockNode implements INode {
 
     @Override
     public void buildString(StringBuilder builder, int tabs) {
-
+        builder.append("BlockNode\n").append(left_curly.toString()).append("\n");
+        tabs++;
+        if (right != null)
+            right.buildString(builder, tabs);
+        builder.append(right_curly.toString()).append("\n");
     }
 }
