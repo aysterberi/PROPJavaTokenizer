6b3a336e6042ed35bb791033105ce853aacd93cd
Billy <chrysophylax@users.noreply.github.com>
2016-11-10 15:27:24 +0100

    ExprNode rewrite

--- src/main/java/prop/assignment0/ExpressionNode.java
+++ src/main/java/prop/assignment0/ExpressionNode.java
@@ -1,5 +1,6 @@
 package prop.assignment0;
 
+
 public class ExpressionNode implements INode {
 	public INode termNode;
 	public Lexeme operator;
@@ -7,44 +8,61 @@ public class ExpressionNode implements INode {
 
 	@Override
 	public Object evaluate(Object[] args) throws Exception {
-		if (termNode != null) {
-			double first = (Double) Double.parseDouble(termNode.evaluate(args).toString());
-			if (expressionNode != null) {
-				double second = (Double) Double.parseDouble(expressionNode.evaluate(args).toString());
-
-				switch (operator.token()) {
-					case ADD_OP:
-						return first + second;
-					case SUB_OP:
-						return first - second;
-					default:
-						assert false : operator.token();
-				}
+		double result = (double) args[1];
+		Token op = (Token) args[2];
+		if (result != 0 && op != null) {
+			args[1] = null;
+			args[2] = null;
+			termNode.evaluate(args);
+			switch (op) {
+				case ADD_OP:
+					args[1] = sum(result, (double) args[1]);
+					break;
+				case SUB_OP:
+					args[1] = sub(result, (double) args[1]);
+					break;
+				default:
+					assert false : operator.token();
 			}
-			return first;
+		}
+		else {
+			termNode.evaluate(args);
+		}
+		if (operator != null)
+		{
+			args[2] = operator.token();
+			expressionNode.evaluate(args);
 		}
 		return null;
 	}
 
-    @Override
-    public void buildString(StringBuilder builder, int tabs) {
-        applyTabs(builder, tabs);
-        tabs++;
-        builder.append("ExpressionNode\n");
-        if (termNode != null)
-            termNode.buildString(builder, tabs);
-        if (operator != null) {
-            applyTabs(builder, tabs);
-            builder.append(operator).
-                    append("\n");
-        }
-        if (expressionNode != null)
-            expressionNode.buildString(builder, tabs);
-    }
+	private double sum(double x, double y) {
+		return x + y;
+	}
 
-    private void applyTabs(StringBuilder builder, int tabs) {
-        for (int i = 0; i < tabs; i++) {
-            builder.append("\t");
-        }
-    }
+	private double sub(double x, double y) {
+		return x - y;
+	}
+
+	@Override
+	public void buildString(StringBuilder builder, int tabs) {
+		applyTabs(builder, tabs);
+		tabs++;
+		builder.append("ExpressionNode\n");
+		if (termNode != null)
+			termNode.buildString(builder, tabs);
+		if (operator != null) {
+			applyTabs(builder, tabs);
+			builder.append(operator).
+					append("\n");
+		}
+		if (expressionNode != null)
+			expressionNode.buildString(builder, tabs);
+	}
+
+	private void applyTabs(StringBuilder builder, int tabs) {
+		for (int i = 0; i < tabs; i++) {
+			builder.append("\t");
+		}
+	}
 }
