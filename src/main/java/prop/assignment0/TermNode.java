953cc48e5a316dbf8240e074b15b40939f941a6f
Billy <chrysophylax@users.noreply.github.com>
2016-11-10 15:35:49 +0100

    TermNode eval

--- src/main/java/prop/assignment0/TermNode.java
+++ src/main/java/prop/assignment0/TermNode.java
@@ -13,41 +13,59 @@ public class TermNode implements INode {
 
 	@Override
 	public Object evaluate(Object[] args) throws Exception {
-		double first = Double.parseDouble(factorNode.evaluate(args).toString());
-		if (operator != null) {
-			//ask for the right term eval
-			double second = Double.parseDouble(termNode.evaluate(args).toString());
-			switch (operator.token()) {
+		double result = (double) args[1];
+		double multiplicand = (double) factorNode.evaluate(args);
+		Token op = (Token) args[2];
+		if (result != 0 && op != null) {
+			args[1] = null;
+			args[2] = null;
+			switch (op) {
 				case MULT_OP:
-					return first * second;
+					args[1] = mult(result, (double) args[1]);
+					break;
 				case DIV_OP:
-					return first / second;
+					args[1] = div(result, (double) args[1]);
+					break;
 				default:
 					assert false : operator.token();
 			}
+		} else {
+			args[1] = multiplicand;
 		}
-		return first;
+		if (operator != null) {
+			args[2] = operator.token();
+			termNode.evaluate(args);
+		}
+		return null;
 	}
 
-    @Override
-    public void buildString(StringBuilder builder, int tabs) {
-        applyTabs(builder, tabs);
-        tabs++;
-        builder.append("TermNode\n");
-        if (factorNode != null)
-            factorNode.buildString(builder, tabs);
-        if (operator != null) {
-            applyTabs(builder, tabs);
-            builder.append(operator).
-                    append("\n");
-        }
-        if (termNode != null)
-            termNode.buildString(builder, tabs);
-    }
+	private double div(double x, double y) {
+		return x / y;
+	}
 
-    private void applyTabs(StringBuilder builder, int tabs) {
-        for (int i = 0; i < tabs; i++) {
-            builder.append("\t");
-        }
-    }
+	private double mult(double x, double y) {
+		return x * y;
+	}
+
+	@Override
+	public void buildString(StringBuilder builder, int tabs) {
+		applyTabs(builder, tabs);
+		tabs++;
+		builder.append("TermNode\n");
+		if (factorNode != null)
+			factorNode.buildString(builder, tabs);
+		if (operator != null) {
+			applyTabs(builder, tabs);
+			builder.append(operator).
+					append("\n");
+		}
+		if (termNode != null)
+			termNode.buildString(builder, tabs);
+	}
+
+	private void applyTabs(StringBuilder builder, int tabs) {
+		for (int i = 0; i < tabs; i++) {
+			builder.append("\t");
+		}
+	}
 }
